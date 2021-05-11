cde853fbe2f8ce8e5eb3c691af2023ab221101f7
lwunruh <lukeunruh@gmail.com>
2021-05-10 19:58:36 -0800

    Reordered creative tab

--- src/main/java/com/github/lwunruh/ToastMod.java
+++ src/main/java/com/github/lwunruh/ToastMod.java
@@ -12,10 +12,28 @@ public class ToastMod implements ModInitializer {
 
 	public static final String MOD_ID = "toastmod";
 
-	public static final ItemGroup Toast = FabricItemGroupBuilder.build(
-			new Identifier(MOD_ID, "toast_items"),
-			() -> new ItemStack(ModItems.MEDIUM_TOAST)
-	);
+	public static final ItemGroup Toast = FabricItemGroupBuilder.create(
+			new Identifier(ToastMod.MOD_ID, "toast_items"))
+			.icon(() -> new ItemStack(ModItems.LIGHT_TOAST))
+			.appendItems(stacks -> {
+				stacks.add(new ItemStack(ModItems.BREAD_KNIFE));
+				stacks.add(new ItemStack(Items.BREAD));
+				stacks.add(new ItemStack(ModItems.BERRY_BREAD));
+				stacks.add(new ItemStack(ModItems.PUMPKIN_BREAD));
+				stacks.add(new ItemStack(ModItems.SLICED_BREAD));
+				stacks.add(new ItemStack(ModItems.SLICED_BERRY_BREAD));
+				stacks.add(new ItemStack(ModItems.SLICED_PUMPKIN_BREAD));
+				stacks.add(new ItemStack(ModItems.LIGHT_TOAST));
+				stacks.add(new ItemStack(ModItems.LIGHT_BERRY_TOAST));
+				stacks.add(new ItemStack(ModItems.LIGHT_PUMPKIN_TOAST));
+				stacks.add(new ItemStack(ModItems.MEDIUM_TOAST));
+				stacks.add(new ItemStack(ModItems.MEDIUM_BERRY_TOAST));
+				stacks.add(new ItemStack(ModItems.MEDIUM_PUMPKIN_TOAST));
+				stacks.add(new ItemStack(ModItems.BURNT_TOAST));
+				stacks.add(new ItemStack(ModItems.BURNT_BERRY_TOAST));
+				stacks.add(new ItemStack(ModItems.BURNT_PUMPKIN_TOAST));
+			})
+			.build();
 
 	@Override
 	public void onInitialize() {
